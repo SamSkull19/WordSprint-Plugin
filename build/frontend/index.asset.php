919ddda66cf6cbe02b0e517d568f55b664c6b58a
SamSkull19 <sifatsamin19@gmail.com>
2026-06-28 17:47:40 +0600

    Packages rechecked

--- build/frontend/index.asset.php
+++ build/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '42579c4bf7d6080ec493');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '8ef88035513b9897d078');
